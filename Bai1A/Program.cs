511a0a8ffd5f8d367ae48fe56091a3b1db30c322
Tichondrius <tichondrius.btv@gmail.com>
2017-07-29 00:28:36 +0700

    export excel in path

--- Bai1A/Program.cs
+++ Bai1A/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ExcelDataReader;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.Dynamic;
@@ -19,64 +20,113 @@ namespace Bai1A
             {
                 using (var reader = ExcelReaderFactory.CreateReader(stream))
                 {
-                    
-                        var columnsLength = reader.FieldCount;
-                        Console.WriteLine(reader.ResultsCount);
-                        int row = 0;
-                        while (reader.Read())
-                        {
-                            var record = new ExpandoObject() as IDictionary<string, Object>;
-                            for (int i = 0; i < columnsLength; i++)
-                            {
-                                if (row == 0)
-                                {
-                                    var header = new TitleHeader();
-                                    header.Field = reader.GetString(i);
-                                    excelData.headers.Add(header);
 
+                    var columnsLength = reader.FieldCount;
+                    Console.WriteLine(reader.ResultsCount);
+                    int row = 0;
+                    while (reader.Read())
+                    {
+                        var record = new ExpandoObject() as IDictionary<string, Object>;
+                        for (int i = 0; i < columnsLength; i++)
+                        {
+                            if (row == 0)
+                            {
+                                var header = new TitleHeader();
+                                header.Field = reader.GetString(i);
+                                excelData.headers.Add(header);
+
+                            }
+                            else
+                            {
+                                if (row == 1)
+                                {
+
+                                    var type = reader.GetFieldType(i).Name;
+                                    switch (type)
+                                    {
+                                        case "String":
+                                            excelData.headers[i].TypeValue = "String";
+                                            record.Add(excelData.headers[i].Field, reader.GetString(i));
+                                            break;
+                                        case "Integer":
+                                        case "Double":
+                                            excelData.headers[i].TypeValue = "Number";
+                                            record.Add(excelData.headers[i].Field, reader.GetDouble(i));
+                                            break;
+                                    }
                                 }
                                 else
                                 {
-                                    if (row >= 1)
+                                    switch (excelData.headers[i].TypeValue)
                                     {
-                                       
-                                        var type = reader.GetFieldType(i).Name;
-                                        switch (type) {
-                                            case "String":
-                                                excelData.headers[i].TypeValue = "String";
-                                                if (reader.IsDBNull(i) == true) {
-                                                    record.Add(excelData.headers[i].Field, null);
-                                                }
-                                                record.Add(excelData.headers[i].Field, reader.GetString(i));
-                                                break;
-                                            case "Integer":
-                                            case "Double":
-                                                excelData.headers[i].TypeValue = "Number";
-                                                if (reader.IsDBNull(i) == true)
-                                                {
-                                                    record.Add(excelData.headers[i].Field, null);
-                                                }
-                                                else record.Add(excelData.headers[i].Field, reader.GetDouble(i));
-                                                break;
-                                          
-                                        }
-                                       
+                                        case "String":
+                                            if (reader.IsDBNull(i) == true)
+                                            {
+                                                record.Add(excelData.headers[i].Field, null);
+                                            }
+                                            else record.Add(excelData.headers[i].Field, reader.GetString(i));
+                                            break;
+                                        case "Number":
+                                            if (reader.IsDBNull(i) == true)
+                                            {
+                                                record.Add(excelData.headers[i].Field, null);
+                                            }
+                                            else record.Add(excelData.headers[i].Field, reader.GetDouble(i));
+                                            break;
                                     }
                                 }
                             }
+                        }
                         if (row != 0) {
-                            excelData.lstRecords.Add(record as ExpandoObject);   
+                            excelData.lstRecords.Add(record as ExpandoObject);
                         }
-                            row++;
-                        }
-                    
+                        row++;
+                    }
+
                 }
             }
             return excelData;
         }
+        static void ExportExcelInPath(String Path, ExcelData excelData)
+        {
+            using (ExcelPackage p = new ExcelPackage())
+            {
+                p.Workbook.Properties.Author = "Miles Dyson";
+                p.Workbook.Properties.Title = "SkyNet Monthly Report";
+                p.Workbook.Properties.Company = "Cyberdyne Systems";
+
+                // The rest of our code will go here...
+                p.Workbook.Worksheets.Add("April 2012");
+                ExcelWorksheet ws = p.Workbook.Worksheets[1]; // 1 is the position of the worksheet
+                ws.Name = "April 2012";
+
+                var lenghtHeader = excelData.headers.Count();
+                var lenghRecord = excelData.lstRecords.Count();
+              
+                //Write header
+                for (int i = 0; i < lenghtHeader; i += 1) {
+                    ws.Cells[1, i + 1].Value = excelData.headers[i].Field;
+                }
+                //Write record
+                for (int i = 0; i < lenghRecord; i += 1) {
+                    for (int j = 0; j < lenghtHeader; j += 1) {
+                        var value = excelData.lstRecords[i] as IDictionary<string, object>;
+                        if (value != null) {
+                            ws.Cells[i + 2, j + 1].Value = value[excelData.headers[j].Field];
+                        }
+                    }
+                }
+
+                Byte[] bin = p.GetAsByteArray();
+                File.WriteAllBytes(@"C:\Report.xlsx", bin);
+            }
+        }
         static void Main(string[] args)
         {
             var excelLoadData = getRecordFromPath("data.xlsx");
+
+            ExportExcelInPath("", excelLoadData);
+
         }
     }
 }
